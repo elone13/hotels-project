7d5b65346e599a1956fa3282e6b41aad9c3c868a
ELONE AIMERICK <eloneaimerick2001@gmail.com>
2025-03-20 13:12:17 +0000

    mise a jour

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Header from "./components/header/Header";
 import Footer from "./components/footer/footer";
-import HomePage from "./pages/home/homepage";
+import HomePage from "./pages/home/Homepage";
 import About from "./pages/about/About";
 import Contact from "./pages/contact/Contact";
-import Erreur from "./pages/erreur/erreur"
+import Erreur from "./pages/erreur/index";
 
 
 const App = () => {
